c15d35f3d73daead287dc7751c5b8fcfc3254330
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-21 10:25:25 -0300

    Add logic isActive style in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,20 @@
 import styles from './style.module.css'
 
 import React from 'react'
+import { useLocation, Link} from 'react-router-dom'
 
 export default function Nav() {
+  const path = useLocation().pathname
+
   return (
     <div className={styles.container}>
-        <button className={styles.active}>Produtos</button>
-        <button>Contato</button>
+      <Link to='/'>
+        <button className={path==='/'?styles.active:''}>Produtos</button>
+      </Link>
+
+      <Link to='/contato'>
+        <button className={path==='/contato'?styles.active:''}>Contato</button>
+      </Link>
     </div>
   )
 }
