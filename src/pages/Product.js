23bf1da47d48b0ab07cba4b32aabe11b60382400
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-21 14:14:15 -0300

    Update product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Layout from '../components/Layout'
+import styles from '../styles/Product.module.css'
 
 export default function Product() {
   const {name} = useParams();
-  console.log(name)
+  const [product,setProduct] = useState(null)
+
+  useEffect(()=>{
+    const fetchData = async() =>{
+      const res = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${name}`)
+      const data = await res.json();
+      console.log(data)
+
+      setProduct(data)
+    }
+
+    if(name){
+      fetchData()
+    }
+  },[name])
 
   return (
     <Layout>
-      <h1>{name}</h1>
+      {
+        product&&(
+          <div className={styles.container}>
+            <img alt={product.nome} src={product.fotos[0].src}/>
+            <div className={styles.infoContainer}>
+              <h1>{product.nome}</h1>
+              <label className={styles.valueBox}>R$ {product.preco}</label>
+              <div className={styles.descriptionContainer}>
+                <p>{product.descricao}</p>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </Layout>
   )
 }
