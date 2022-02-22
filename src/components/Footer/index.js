9df7f1192ff2a3a7aeee07e0ed44db7db0c04455
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-22 14:56:34 -0300

    Change footer text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className={styles.container}>
-        <label>Feito com amor por <strong><a href='https://github.com/joaopedroalb'>João Pedro Albuquerque</a></strong></label>
+        <label>Feito com ❤️ por <strong><a href='https://github.com/joaopedroalb'>João Pedro Albuquerque</a></strong></label>
     </footer>
   )
 }
